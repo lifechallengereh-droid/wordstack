b165f4b3f8116740332d7849cafa9429203c3160
lifechallengereh-droid <lifechallenger.eh@gmail.com>
2026-08-25 06:13:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wordstack-v7-2-6-6-buttons';
-const CORE=['./','./index.html','./styles.css?v=7266','./app.js?v=7266','./google-sync.js?v=7266','./manifest.webmanifest'];
+const CACHE='wordstack-v7-2-6-7-fonts';
+const CORE=['./','./index.html','./styles.css?v=7267','./app.js?v=7267','./google-sync.js?v=7267','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(x=>c.add(x)))));});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]));});
 self.addEventListener('fetch',e=>{if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));return;}e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put(e.request,cp)).catch(()=>{});return r;}).catch(()=>caches.match(e.request)));});
