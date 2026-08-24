434e96f05a23c6b0c2f02c792bcc311d62c9d285
lifechallengereh-droid <lifechallenger.eh@gmail.com>
2026-08-25 00:42:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wordstack-v7-2-6-memorytoggle';
+const CACHE='wordstack-v7-2-6-1-spacing';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./google-sync.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(ASSETS.map(a=>c.add(a))))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
